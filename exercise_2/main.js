8370bbeccbad91047a404e90efc206d0a8f0f6d4
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-09-05 13:01:10 +0800

    exercise 2

--- exercise_2/main.js
+++ exercise_2/main.js
@@ -8,16 +8,24 @@ for (var r of rows) {
     // console.log(itemRow);
     itemName = r.cells[0].innerText;
     price = r.cells[1].innerText;
+    button = r.cells[2]
+    // console.log(button)
+    button.addEventListener('click', addItem)
     // console.log("Item: " + itemName + " Price: $" + price);
-    console.log(itemRow[2])
+    // console.log(itemRow[2])
     // itemRow[2].onclick = function() {
 
     // }
 }
 
-// 1. how to access names and values from html 
-// 2. 
-function addItem(e) {
-    const el = e.eventTarget
+function addItem() {
+    let itemInputCell = event.target;
+    let itemNameCell = event.target.parentElement.parentElement.children[0];
+    let itemPriceCell = event.target.parentElement.parentElement.children[1];
+    console.log(itemPriceCell);
+    let itemRow = document.createElement('tr');
+    itemRow.appendChild(itemNameCell.cloneNode(true));
+    itemRow.appendChild(itemPriceCell.cloneNode(true));
+    shoppingCart.appendChild(itemRow);
     
 }
